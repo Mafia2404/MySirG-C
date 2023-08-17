a2915a43f4e32f82d4393bbeec0e9701b12a4dde
Uddhav Pawar <93379977+Uddhav2404@users.noreply.github.com>
2023-08-17 17:19:44 +0530

    Add files via upload

---
+++ Assignment6/assignment 6/q3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x;
+    x=10>8>4;//10>8 true =1>4 false x=0
+    printf("%d",x);//giving zero 
+    return 0;
+}
---
+++ Assignment6/assignment 6/q4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x;
+    x=!2>0;//2 is true !2 is false =0 then 0>-2 is true x=1
+    printf("%d",x);//returns 1 condition true
+    return 0;
+}
